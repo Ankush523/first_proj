93136aa9e74c2de0195229bd78dab3485b7ab9a6
Ankush Dutta <ankush.ad050203@gmail.com>
2022-01-26 21:45:07 +0530

    yes

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext con) {
     return MaterialApp(home: Scaffold(
       appBar: AppBar(title: Text('My First App'),),
-      body: Text('This is my default text!'),
-    ),);
+      body: Column(
+        children: [
+          Text('The Question!'),
+          RaisedButton(child: Text('Answer 1'), onPressed: null),
+          RaisedButton(child: Text('Answer 2'), onPressed: null),
+          RaisedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
+    ),
+  );
   }
 }
